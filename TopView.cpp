ee9f4e4356c67f5b60637967ded33442a97829e2
Adwitiya Mourya <adwitiya.code@gmail.com>
2026-05-24 18:25:32 +0530

    TopView Vertical Order Approach

---
+++ TopView.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+  struct TreeNode {
+      int data;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : data(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}    
+  };
+
+class Solution{
+    public:
+    vector<int> topView(TreeNode *root){
+        vector<int> ans;
+        map<int, int> mp;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+
+        while(!q.empty()){
+            int line = q.front().second;
+            TreeNode* node = q.front().first;
+
+            q.pop();
+            if(mp.find(line) == mp.end()) mp[line] = node->data;
+
+            if(node->left) q.push({node->left, line - 1});
+            if(node->right) q.push({node->right, line + 1});
+        }
+
+        for(auto it : mp) 
+            ans.push_back(it.second);
+        return ans;
+    }
+  };
